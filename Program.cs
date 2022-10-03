36b6e7a59309ac115b5e2aa3b1f6d5caa9693feb
Duncan Caruthers <duncan.caruthers01@gmail.com>
2022-10-03 11:08:58 -0400

    Created Login Handler

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using Login;
+using Users;
+
+LoginHandler handler = new LoginHandler();
+handler.addUser("test", "test", true);
+
+User? test = handler.login("test", "test");
+if (test != null)
+{
+    if (test.IsManager)
+    {
+        Console.WriteLine("It works!");
+    }
+}
