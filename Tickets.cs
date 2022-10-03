2d29e46def8d66a74d9611deac7a7ebe70e2a43e
Duncan Caruthers <duncan.caruthers01@gmail.com>
2022-10-03 12:10:19 -0400

    Planning comments

---
+++ Tickets.cs
@@ -0,0 +1,6 @@
+/*
+    Planning for User Tickets:
+        Class Ticket -> stores ticket information (amount,description,status,who made it)
+        Class TIcketHandler -> Manages ticket database
+*/
+
