80ec17cb4c724ec80766429c08285a7e8693f803
Duncan Caruthers <duncan.caruthers01@gmail.com>
2022-10-04 16:39:29 -0400

    Created User class

---
+++ Models/Users.cs
@@ -0,0 +1,26 @@
+namespace Users;
+
+public class User
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public bool IsManager { get; set; }
+
+    public User(string username, string password, bool mgr)
+    {
+        Username = username;
+        Password = password;
+        IsManager = mgr;
+    }
+
+    public bool checkCredentials(string username, string password)
+    {
+        if (Username.Equals(username) && Password.Equals(Password))
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+}
