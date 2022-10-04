df44ba1093d107a3992f93c47fff139f48ab2e3c
Duncan Caruthers <duncan.caruthers01@gmail.com>
2022-10-04 16:56:53 -0400

    Refactoring UI functions into a class

--- UI/Program.cs
+++ UI/Program.cs
@@ -14,119 +14,6 @@
 
 using Users;
 
-static int GetSelection()
-{
-    string? selection = Console.ReadLine();
-    if (selection != null)
-    {
-        int value;
-        bool check = int.TryParse(selection, out value);
-        if (check)
-        {
-            return value;
-        }
-    }
-    return -1;
-}
-
-static double GetDouble()
-{
-    string? selection = Console.ReadLine();
-    if (selection != null)
-    {
-        double value;
-        bool check = double.TryParse(selection, out value);
-        if (check)
-        {
-            return value;
-        }
-    }
-    return -1;
-}
-
-static void ManagerInteraction()
-{
-    Console.WriteLine("Welcome Manager!");
-    Console.WriteLine("----------------------------------------");
-    Console.WriteLine(" [1] View list");
-    Console.WriteLine(" [2] Exit");
-    Console.WriteLine("----------------------------------------");
-    int selection = GetSelection();
-    if (selection == 1)
-    {
-        Console.WriteLine("Yee haw! this section of the system does not yet exist");
-    }
-    else if (selection == 2)
-    {
-        Environment.Exit(0);
-    }
-    else
-    {
-        Console.WriteLine("Invalid selection");
-    }
-}
-
-static void EmployeeInteraction()
-{
-    Console.WriteLine("Welecome Employee!");
-    Console.WriteLine("----------------------------------------");
-    Console.WriteLine(" [1] View Past Tickets");
-    Console.WriteLine(" [2] Create Reimbursement Ticket");
-    Console.WriteLine(" [3] Exit");
-    Console.WriteLine("----------------------------------------");
-    int selection = GetSelection();
-    if (selection == 1)
-    {
-        Console.WriteLine("Yee haw! this part of the system does not exist yet");
-    }
-    else if (selection == 2)
-    {
-        Console.Write("Reimbursement Amount: ");
-        double amt = GetDouble();
-        Console.Write("Description of Ticket: ");
-        string? desc = Console.ReadLine();
-        if (desc == null)
-        {
-            Console.WriteLine("Null description");
-        }
-        Console.WriteLine("Cant Do anything with this yet, but heres what you entered");
-        Console.WriteLine(amt);
-        Console.WriteLine(desc);
-    }
-    else if (selection == 3)
-    {
-        Environment.Exit(0);
-    }
-    else
-    {
-        Console.WriteLine("Invalid selection");
-    }
-
-}
-
-static void LoginInteraction()
-{
-    User temp = new User("test", "test", false);
-    Console.Write("Username: ");
-    string? username = Console.ReadLine();
-    Console.Write("Password: ");
-    string? password = Console.ReadLine();
-    if (username == null || password == null)
-    {
-        return;
-    }
-    if (temp.checkCredentials(username, password))
-    {
-        if (temp.IsManager)
-        {
-            ManagerInteraction();
-        }
-        else
-        {
-            EmployeeInteraction();
-        }
-    }
-}
 
 
 while (true)
@@ -137,15 +24,15 @@ while (true)
     Console.WriteLine(" [2] Register");
     Console.WriteLine(" [3] Exit");
     Console.WriteLine("----------------------------------------");
-    int selection = GetSelection();
+    int selection = UIHandler.GetSelection();
     if (selection == 1)
     {
-        LoginInteraction();
+        UIHandler.LoginInteraction();
     }
     else if (selection == 2)
     {
         // Employee
-        EmployeeInteraction();
+        UIHandler.RegisterInteraction();
     }
     else if (selection == 3)
     {
@@ -155,5 +42,146 @@ while (true)
     else
     {
         Console.WriteLine("Invalid selection");
+    }
+}
+
+public class UIHandler
+{
+    public static int GetSelection()
+    {
+        string? selection = Console.ReadLine();
+        if (selection != null)
+        {
+            int value;
+            bool check = int.TryParse(selection, out value);
+            if (check)
+            {
+                return value;
+            }
+        }
+        return -1;
+    }
+
+    public static double GetDouble()
+    {
+        string? selection = Console.ReadLine();
+        if (selection != null)
+        {
+            double value;
+            bool check = double.TryParse(selection, out value);
+            if (check)
+            {
+                return value;
+            }
+        }
+        return -1;
+    }
+
+    public static void ManagerInteraction()
+    {
+        Console.WriteLine("Welcome Manager!");
+        Console.WriteLine("----------------------------------------");
+        Console.WriteLine(" [1] View list");
+        Console.WriteLine(" [2] Exit");
+        Console.WriteLine("----------------------------------------");
+        int selection = GetSelection();
+        if (selection == 1)
+        {
+            Console.WriteLine("Yee haw! this section of the system does not yet exist");
+        }
+        else if (selection == 2)
+        {
+            Environment.Exit(0);
+        }
+        else
+        {
+            Console.WriteLine("Invalid selection");
+        }
+    }
+
+    public static void EmployeeInteraction()
+    {
+        Console.WriteLine("Welecome Employee!");
+        Console.WriteLine("----------------------------------------");
+        Console.WriteLine(" [1] View Past Tickets");
+        Console.WriteLine(" [2] Create Reimbursement Ticket");
+        Console.WriteLine(" [3] Exit");
+        Console.WriteLine("----------------------------------------");
+        int selection = GetSelection();
+        if (selection == 1)
+        {
+            Console.WriteLine("Yee haw! this part of the system does not exist yet");
+        }
+        else if (selection == 2)
+        {
+            Console.Write("Reimbursement Amount: ");
+            double amt = GetDouble();
+            Console.Write("Description of Ticket: ");
+            string? desc = Console.ReadLine();
+            if (desc == null)
+            {
+                Console.WriteLine("Null description");
+            }
+            Console.WriteLine("Cant Do anything with this yet, but heres what you entered");
+            Console.WriteLine(amt);
+            Console.WriteLine(desc);
+        }
+        else if (selection == 3)
+        {
+            Environment.Exit(0);
+        }
+        else
+        {
+            Console.WriteLine("Invalid selection");
+        }
+
+    }
+
+    public static void LoginInteraction()
+    {
+        User temp = new User("test", "test", false);
+        Console.Write("Username: ");
+        string? username = Console.ReadLine();
+        Console.Write("Password: ");
+        string? password = Console.ReadLine();
+        if (username == null || password == null)
+        {
+            return;
+        }
+        if (temp.checkCredentials(username, password))
+        {
+            if (temp.IsManager)
+            {
+                ManagerInteraction();
+            }
+            else
+            {
+                EmployeeInteraction();
+            }
+        }
+    }
+
+    public static void RegisterInteraction()
+    {
+        User temp = new User("test", "test", false);
+        Console.Write("Username: ");
+        string? username = Console.ReadLine();
+        Console.Write("Password: ");
+        string? password = Console.ReadLine();
+        if (username == null || password == null)
+        {
+            return;
+        }
+        if (temp.checkCredentials(username, password))
+        {
+            if (temp.IsManager)
+            {
+                ManagerInteraction();
+            }
+            else
+            {
+                EmployeeInteraction();
+            }
+        }
     }
 }
