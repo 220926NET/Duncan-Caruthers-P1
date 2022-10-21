0331a473e044da6051c3cd719dae4578290f7eab
Duncan Caruthers <duncan.caruthers01@gmail.com>
2022-10-21 10:40:17 -0400

    A little project for helping setup for testing

---
+++ CreateAdmin/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using Models;
+using DataAccess;
+using Services;
+
+User admin = new User("admin", "", true);
+admin.Password = User.Hash("admin", admin.Salt);
+
+LoginHandler handeler = new LoginHandler(new DatabaseStorage());
+handeler.AddUser(admin);
